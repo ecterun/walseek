171aeaef622e27618f39429a9f5e4f07d2f425ec
Matthew Felten <Ecterun27@gmail.com>
2017-12-12 22:21:09 -0600

    added the check_compare function, and run it on specific stores

--- walseek.py
+++ walseek.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
-import requests, json, datetime, subprocess, os
+import requests, json, datetime, subprocess, os, time
 from subprocess import call, Popen
-
+from time import strftime
 def onlinelookup(itemid):
     #itemid = '951814057'
     now = datetime.datetime.now()
@@ -11,10 +11,14 @@ def onlinelookup(itemid):
         apikey = keyfile.read().strip('\n')
     r = requests.get('http://api.walmartlabs.com/v1/items/%s?format=json&apiKey=%s' % (itemid, apikey))
 
-    if r.status_code == 200:
-        with open('/opt/walmart/python/data/online/webData-%s-%s.json' % (itemid,datestamp), 'w') as outfile:
+    #if r.status_code == 200:
+    with open('/opt/walmart/python/data/online/%s/webData-%s-%s.json' % (datestamp,itemid,datestamp), 'w') as outfile:
+        if r.status_code == 200:
             json.dump(r.json(), outfile)
             print "writing to %s" % outfile.name
+        else:
+            json.dump('{}', outfile)
+            print "writing empty file %s" % outfile.name
     #else print to error logs
 
 def local_query(storenum, query='LEGO'):
@@ -35,11 +39,8 @@ def local_query(storenum, query='LEGO'):
     call("jq --slurp '[.[].results[]]' /opt/walmart/python/data/local/local_query-" + storenum + "*.json > /opt/walmart/python/data/local/full_query-" + storenum + "-" + datestamp + ".json", shell=True )
 
 
-def get_local_item_data(itemid,storenum):
-    now = datetime.datetime.now()
-    datestamp = str(now.month) + str(now.day)
-
-    localfile = '/opt/walmart/python/data/local/full_query-%s-%s.json' % (storenum,datestamp)
+def get_local_item_data(itemid, storenum, strdate=strftime("%m%d")):
+    localfile = '/opt/walmart/python/data/local/full_query-%s-%s.json' % (storenum,strdate)
     if not os.path.isfile(localfile):
         local_query(storenum)
 
@@ -68,7 +69,7 @@ def get_online_item_data(itemid):
     now = datetime.datetime.now()
     datestamp = str(now.month) + str(now.day)
 
-    onlinefile = '/opt/walmart/python/data/online/webData-%s-%s.json' % (itemid, datestamp)
+    onlinefile = '/opt/walmart/python/data/online/%s/webData-%s-%s.json' % (datestamp, itemid, datestamp)
     if not os.path.isfile(onlinefile):
         onlinelookup(itemid)
 
@@ -122,7 +123,7 @@ def compare_item_data(itemid, storenum):
             'msrpInCents': msrpcents,
             'salePriceInCents': salepricecents
             },
-            'localDiscount': "%.2f" % round(discount,2),
+            'localDiscount': "%.f" % round(discount,2),
             'localQuantity': localdata['quantity'],
             'modelNumber': onlinedata['modelNumber'],
             'url': localdata['url']
@@ -134,6 +135,45 @@ def compare_item_data(itemid, storenum):
         print "writing to %s" % outfile.name
 
 
+def compare_store(storenum):
+    now = datetime.datetime.now()
+    datestamp = str(now.month) + str(now.day)
+
+    localfile = '/opt/walmart/python/data/local/full_query-%s-%s.json' % (storenum,datestamp)
+    if not os.path.isfile(localfile):
+        local_query(storenum)
+
+    cmd = Popen("jq -r '.[].productId.WWWItemId' " + localfile, stdout=subprocess.PIPE,shell=True)
+    cmd_out, cmd_err = cmd.communicate()
+
+    for line in cmd_out.splitlines():
+        compare_item_data(line, storenum)
+
+def check_compare_data(storenum):
+    now = datetime.datetime.now()
+    datestamp = str(now.month) + str(now.day)
+    yesterday = str(now.month) + str(now.day -1)
+
+    currentcompare = '/opt/walmart/python/data/compare/compare-%s-%s.json' % (storenum, datestamp)
+    previouscompare = '/opt/walmart/python/data/compare/compare-%s-%s.json' % (storenum, yesterday)
+
+    cmd = Popen("jq -c 'select(.localDiscount!=\"0\")|.' " + currentcompare, stdout=subprocess.PIPE,shell=True)
+    cmd_out, cmd_err = cmd.communicate()
+    print 'Comparing StoreNum:%s data from %s and %s.' % (storenum, datestamp, yesterday)
+    for line in cmd_out.splitlines():
+        curjson = json.loads(line)
+        itemid = curjson['itemId']
+        scmd = Popen("jq -c 'select(.itemId==\"" + itemid + "\")' " + previouscompare + " |head -n 1", stdout=subprocess.PIPE,shell=True)
+        scmd_out, scmd_err = scmd.communicate()
+        if scmd_out == '':
+            prevprice = -1
+        else:
+            prevjson = json.loads(scmd_out)
+            prevprice = prevjson['price']['localPriceInCents']
+        curprice = curjson['price']['localPriceInCents']
+        pricelowered = True if curprice < prevprice else False
+        if pricelowered:
+            print 'StoreNum:%s ItemId:%s CurrentPrice:%s PreviousPrice:%s' % (storenum, itemid, curprice, prevprice)
 
 def main():
     print ("Running pricechecker")
@@ -144,10 +184,34 @@ def main():
     #get_local_item_data('946006306', '5669')
     #get_online_item_data('951814057')
 
-    compare_item_data('951814057', '5669')
-    compare_item_data('946006306', '5669')
-    compare_item_data('799194178', '5669')
+#Test compares
+    #compare_item_data('951814057', '5669')
+    #compare_item_data('946006306', '5669')
+    #compare_item_data('799194178', '5669')
 
+#Full Local Compare
+    compare_store('1294')
+    compare_store('1515')
+    compare_store('1551')
+    compare_store('2452')
+    compare_store('2828')
+    compare_store('2936')
+    compare_store('5438')
+    compare_store('5668')
+    compare_store('5669')
+    compare_store('6394')
+
+#Full Check Compare Data
+    check_compare_data('1294')
+    check_compare_data('1515')
+    check_compare_data('1551')
+    check_compare_data('2452')
+    check_compare_data('2828')
+    check_compare_data('2936')
+    check_compare_data('5438')
+    check_compare_data('5668')
+    check_compare_data('5669')
+    check_compare_data('6394')
 
 #how to parse json
 #json_str = r.json()
